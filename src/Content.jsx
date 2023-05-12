13a5d6e6d4ef929ead932949715a3aef282cf816
RobynSpaulding <robynm27@gmail.com>
2023-05-11 20:39:23 -0700

    creates Signup.jsx and imports it in content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
+import { Signup } from "./Signup";
+
 export function Content() {
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <h1>Welcome to the Writers Tool!</h1>
+      <Signup />
     </div>
   );
 }
