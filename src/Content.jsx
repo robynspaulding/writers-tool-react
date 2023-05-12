9332a181ccaef50f9f3c4fe24a7f0dbee623454c
RobynSpaulding <robynm27@gmail.com>
2023-05-11 20:47:37 -0700

    created LogoutLink.jsx file with working logout out logic, render in content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,4 +1,5 @@
 import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 import { Signup } from "./Signup";
 
 export function Content() {
@@ -7,6 +8,7 @@ export function Content() {
       <h1>Welcome to the Writers Tool!</h1>
       <Signup />
       <Login />
+      <LogoutLink />
     </div>
   );
 }
