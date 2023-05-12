08b0ebbfd7bb4cad95b7766b3d9a5429f55a8d5e
RobynSpaulding <robynm27@gmail.com>
2023-05-11 20:46:15 -0700

    created Login.jsx file renders in content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,3 +1,4 @@
+import { Login } from "./Login";
 import { Signup } from "./Signup";
 
 export function Content() {
@@ -5,6 +6,7 @@ export function Content() {
     <div>
       <h1>Welcome to the Writers Tool!</h1>
       <Signup />
+      <Login />
     </div>
   );
 }
