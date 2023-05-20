0fdf07f4af5b7aa62b02986e09c35fcf3e16a1b8
RobynSpaulding <robynm27@gmail.com>
2023-05-19 20:37:12 -0700

    adds form to projectsNew

--- src/ProjectsNew.jsx
+++ src/ProjectsNew.jsx
@@ -2,6 +2,15 @@ export function ProjectsNew() {
   return (
     <div>
       <h1>New Project</h1>
+      <form>
+        <div>
+          Working Title: <input name="working_title" type="text" />
+        </div>
+        <div>
+          Status: <input name="status" type="text" />
+        </div>
+        <button type="submit">Create project</button>
+      </form>
     </div>
   );
 }
