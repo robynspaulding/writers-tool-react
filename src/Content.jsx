0c530b78062798fea54cf418204619075908c4c3
RobynSpaulding <robynm27@gmail.com>
2023-05-19 19:56:11 -0700

    create projectsIndex file with short message, inports projectsIndex in Content file - working; adds code to handleIndexProjects with useState and axios request to api

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,29 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
+import { ProjectsIndex } from "./ProjectsIndex";
 import { Signup } from "./Signup";
 
 export function Content() {
+  const [projects, setProjects] = useState([]);
+
+  const handleIndexProjects = () => {
+    axios.get("http://localhost:3000/projects.json").then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  };
+
+  useEffect(handleIndexProjects, []);
   return (
     <div>
       <h1>Welcome to the Writers Tool!</h1>
       <Signup />
       <Login />
       <LogoutLink />
+      <ProjectsIndex projects={projects} />
     </div>
   );
 }
---
+++ src/ProjectsIndex.jsx
@@ -0,0 +1,7 @@
+export function ProjectsIndex() {
+  return (
+    <div>
+      <h1> All Projects!! </h1>
+    </div>
+  );
+}
