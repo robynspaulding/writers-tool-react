0ed41a12b008143aec7aeaf85dd066df83469edb
RobynSpaulding <robynm27@gmail.com>
2023-05-19 20:18:15 -0700

    installs react router dom, adds browserRouter, router and routes and implements in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <Content />
+
+      <Routes>
+        <Route path="/" element={<Content />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
