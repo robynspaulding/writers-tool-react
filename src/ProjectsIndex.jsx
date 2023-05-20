7e6019fe055fab26576832c86b3fdea3ffd21021
RobynSpaulding <robynm27@gmail.com>
2023-05-19 20:04:52 -0700

    implements map in projectIndex to display each projects name and status

--- src/ProjectsIndex.jsx
+++ src/ProjectsIndex.jsx
@@ -1,7 +1,13 @@
-export function ProjectsIndex() {
+export function ProjectsIndex(props) {
   return (
-    <div>
+    <div id="projects-index">
       <h1> All Projects!! </h1>
+      {props.projects.map((project) => (
+        <div key={project.id}>
+          <h2>Title: {project.working_title}</h2>
+          <p>Status: {project.status}</p>
+        </div>
+      ))}
     </div>
   );
 }
